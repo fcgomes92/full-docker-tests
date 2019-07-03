38f5df2b5dd1b423dd9953ad4193f2fb4c173825
Fernando Gomes <fcgomes.92@gmail.com>
2019-07-03 08:11:30 -0300

    add gitignore

--- src/server.js
+++ src/server.js
@@ -1,9 +1,15 @@
 import express from "express";
 import bodyParser from "body-parser";
 
-const app = express({ port: process.env.PORT });
+const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  return res.status(200).json({ msg: "ok" });
+});
+
+app.listen({ port: process.env.PORT })
 
 export default app;
